9ae36e807480383cecdde084074356d69c6f8817
sanga <bsa1king.naver.com>
2019-04-17 00:09:15 +0900

    fist hotfix commit

--- test_solution/test_app/Form1.cs
+++ test_solution/test_app/Form1.cs
@@ -19,7 +19,7 @@ namespace test_app
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello World!");
+            MessageBox.Show("Hello World! hotfix");
         }
     }
 }
